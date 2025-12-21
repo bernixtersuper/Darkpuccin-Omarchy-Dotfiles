97a86f8927f999b0dcc3405228e048c5897df303
Bernardo Ortiz <163164892+bernixtersuper@users.noreply.github.com>
2025-12-21 02:34:12 -0300

    Add files via upload

---
+++ waybar/brightness-control.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# --- CONFIURATION ---
+DISPLAY_NUM=2
+STEP=5
+STATE_FILE="/tmp/waybar_brightness.tmp"
+# ---------------------
+
+# Function to send the actual DDC/CI command in the background
+set_brightness_in_background() {
+    pkill -f "ddcutil.*setvcp 10"
+    (ddcutil --display $DISPLAY_NUM setvcp 10 $1) &
+}
+
+if [ ! -f "$STATE_FILE" ]; then
+    initial_brightness=$(ddcutil --display $DISPLAY_NUM getvcp 10 -t | cut -d ' ' -f 4)
+    echo "$initial_brightness" > "$STATE_FILE"
+fi
+
+
+current=$(tail -1 "$STATE_FILE" | grep -oE '[0-9]+')
+if [ -z "$current" ]; then
+    current=100 # Default fallback value
+fi
+
+case "$1" in
+    "get")
+        echo " $current"
+        ;;
+    "up")
+        new_brightness=$((current + STEP > 100 ? 100 : current + STEP))
+        if [ "$current" -ne "$new_brightness" ]; then
+            echo "$new_brightness" > "$STATE_FILE"
+            set_brightness_in_background "$new_brightness"
+        fi
+        pkill -RTMIN+1 waybar # Can add whatever RTMIN+n you please, as long as it's an integer. Make sure the signals match between the config and script.
+        ;;
+    "down")
+        new_brightness=$((current - STEP < 0 ? 0 : current - STEP))
+        if [ "$current" -ne "$new_brightness" ]; then
+            echo "$new_brightness" > "$STATE_FILE"
+            set_brightness_in_background "$new_brightness"
+        fi
+        pkill -RTMIN+1 waybar
+        ;;
+    "right_click")
+	new_brightness=0
+	if [ "$current" -ne "$new_brightness" ]; then
+            echo "$new_brightness" > "$STATE_FILE"
+            set_brightness_in_background "$new_brightness"
+        fi
+        pkill -RTMIN+1 waybar
+        ;;
+    "left_click")
+	new_brightness=100
+     	if [ "$current" -ne "$new_brightness" ]; then
+            echo "$new_brightness" > "$STATE_FILE"
+            set_brightness_in_background "$new_brightness"
+        fi
+        pkill -RTMIN+1 waybar
+        ;;
+esac
